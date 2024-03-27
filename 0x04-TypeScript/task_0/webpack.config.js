58665a205e06c214dad1e7db484263fd81287e9f
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-27 16:20:50 +0300

    this is a webpack configuration

--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,12 +1,37 @@
-{
-        "compilerOptions": {
-            "outDir": "./dist/",
-            "sourceMap": true,
-            "noImplicitAny": true,
-            "module": "es6",
-            "target": "es5",
-            "allowJs": true,
-            "moduleResolution": "node",
-                "skipLibCheck": true
-        };
-    }
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: "./js/main.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
