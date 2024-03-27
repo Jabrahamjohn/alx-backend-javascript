9812b5915c06217574176e1c3db0720a3eb74c2d
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-27 17:17:19 +0300

    the major change happening in this proportion is something i dont get

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,44 +1,8 @@
-class Teacher {
-    private readonly firstName: string;
-    private readonly lastName: string;
-    private readonly fullTimeEmployee: boolean;
-    private readonly yearsOfExperience?: number;
-    private readonly location: string;
-    [key: string]: any;
-
-    constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, location: string, yearsOfExperience?: number) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullTimeEmployee = fullTimeEmployee;
-        this.location = location;
-        this.yearsOfExperience = yearsOfExperience;
-    }
-
-
-    // Getter methods
-    getFirstName(): string {
-        return this.firstName;
-    }
-
-
-    getLastName(): string {
-        return this.lastName;
-    }
-
-    getFullTimeEmployee(): boolean {
-        return this.fullTimeEmployee;
-    }
-
-    getYearsOfExperience(): number | undefined {
-        return this.yearsOfExperience;
-    }
-
-    getLocation(): string {
-        return this.location;
-    }
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propNAME: string]: any;
 }
-
-let teacher = new Teacher('John', 'Doe', true, 'New York', 5);
-teacher.contract = true;
-
-
