e55645a4f795c3bde655de3662173e2bd65c9cda
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-27 23:54:32 +0300

    this new not working

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,9 +1,12 @@
 /// <reference path="./crud.d.ts" />
 
-import { RowID, RowElement } from "./interface";
+import { RowID, RowElement, CRUD } from "./interface"; // Import the CRUD object from the interface module
 import { insertRow, deleteRow, updateRow } from "./crud";
 
 const row: RowElement = { firstName: "Guillaume", lastName: "Salva" };
+const newRowID: RowID = CRUD.insertRow(row); // Use the CRUD object to call the insertRow method
 const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
 
-const newRowID: RowID = insertRow(row);
+CRUD.updateRow(newRowID, updatedRow); // Use the CRUD object to call the updateRow method
+CRUD.deleteRow(newRowID); // Use the CRUD object to call the deleteRow method
+
