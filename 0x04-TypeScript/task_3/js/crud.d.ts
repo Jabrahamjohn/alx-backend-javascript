afb0e0601aac3f2fe9093659b38233b3e0e720e7
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-27 23:35:35 +0300

    declaration of what is to  happen

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,2 +1,5 @@
 import { RowID, RowElement } from "./interface";
 
+export declare function insertRow(row: RowElement): number;
+export declare function deleteRow(rowID: RowID): void;
+export declare function updateRow(rowID: RowID, row: RowElement): number;
