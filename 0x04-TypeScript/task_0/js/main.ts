1891790c55dc56ca929d37b814536cf798c3c82d
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-27 16:12:02 +0300

    this is using vanilla js to create a table and elements in them

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,3 +20,29 @@ const student2: Student = {
 };
 
 const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+
+studentsList.forEach(student => {
+    const row = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = student.firstName;
+    row.appendChild(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = student.lastName;
+    row.appendChild(lastNameCell);
+
+    const ageCell = document.createElement("td");
+    ageCell.textContent = student.age.toString();
+    row.appendChild(ageCell);
+
+    const locationCell = document.createElement("td");
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+
+    table.appendChild(row);
+});
+
+document.body.appendChild(table);
