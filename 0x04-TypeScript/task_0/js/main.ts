276cebc92f0dc255bf65fff9def919f4d3d29668
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-27 16:10:05 +0300

    this is how we create an array with variables

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -4,3 +4,19 @@ interface Student {
     age: number;
     location: string;
 }
+
+const student1: Student = {
+    firstName: "John",
+    lastName: "Abraham",
+    age: 28,
+    location: "New York"
+};
+
+const student2: Student = {
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 22,
+    location: "Los Angeles"
+};
+
+const studentsList: Student[] = [student1, student2];
