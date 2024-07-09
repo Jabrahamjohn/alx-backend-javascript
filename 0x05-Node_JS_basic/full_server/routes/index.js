ba225b4e50f7a459417bfd14c8339a628c09907a
Abraham John <jabrahamjohns@gmail.com>
2024-07-09 15:23:28 -0700

    reshaping the server again

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -5,7 +5,7 @@ import StudentsController from '../controllers/StudentsController';
  * Binds the routes to the appropriate handler in the
  * given Express application.
  * @param {Express} app The Express application.
- * @author Abraham john <https://github.com/jabrahamjohn>
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 const mapRoutes = (app) => {
   app.get('/', AppController.getHomepage);
